8842a0a51894fc8f6ab30f7d69305d3403ddab98
Sneha Vishwakarma <88725034+snehavish595@users.noreply.github.com>
2025-04-09 22:56:36 +0530

    working on login functionality

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,6 @@ from .views import run_code, RegisterView, login
 
 urlpatterns = [
     path("api/runCode/", run_code),
-    path("register/", RegisterView.as_view(), name="register"),
-    path("login/", login, name="login"),
+    path("api/register/", RegisterView.as_view(), name="register"),
+    path("api/login/", login, name="login"),
 ]
